3f8b6f4ba3a6e6ccf59bc66c3c9d1431a33a8463
Alejandra Camacho <camacho.ale94@gmail.com>
2020-11-24 01:16:45 -0600

    Forms with reactive components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
-import { FormsModule } from '@angular/forms'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
@@ -11,7 +11,13 @@ import { HttpClientModule } from '@angular/common/http'
 
 @NgModule({
   declarations: [AppComponent, TemplateComponent, ReactiveComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-reactive',
@@ -6,10 +7,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reactive.component.styl']
 })
 export class ReactiveComponent implements OnInit {
+  forma: FormGroup
+  patt = '[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$'
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) {
+    this.createForm()
   }
 
+  ngOnInit(): void {
+    console.log('object')
+  }
+
+  get nameInvalid(): boolean {
+    return this.forma.get('name').invalid && this.forma.get('name').touched
+  }
+
+  get lastInvalid(): boolean {
+    return this.forma.get('last').invalid && this.forma.get('last').touched
+  }
+
+  get mailInvalid(): boolean {
+    return this.forma.get('mail').invalid && this.forma.get('mail').touched
+  }
+
+  createForm(): void {
+    this.forma = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(4)]],
+      last: ['', Validators.required],
+      mail: ['', [Validators.required, Validators.pattern(this.patt)]],
+      address: this.fb.group({
+        street: ['', Validators.required],
+        city: ['', Validators.required]
+      })
+    })
+  }
+
+  save(): void {
+    if (this.forma.invalid) {
+      return Object.values(this.forma.controls).forEach((control) => {
+        control.markAsTouched()
+      })
+    }
+  }
 }
